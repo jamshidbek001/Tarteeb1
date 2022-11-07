4e32a3fda6536935081560c9954677e13184b445
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-11-07 22:12:26 +0100

    INFRA: Initialize Api Project

---
+++ Tarteeb1.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//==================================================
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tarteeb1.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetHomeMessage() => "Tarteeb is running...";
+    }
+}
